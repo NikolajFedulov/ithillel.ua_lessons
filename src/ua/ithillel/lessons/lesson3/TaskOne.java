284cd8e1085b208d8be9ccadee69cdc80478c86f
Nikolaj Fedulov <v20nickf1@gmail.com>
2022-07-27 17:39:29 +0300

    Lesson #3. Text change

--- src/ua/ithillel/lessons/lesson3/TaskOne.java
+++ src/ua/ithillel/lessons/lesson3/TaskOne.java
@@ -2,18 +2,53 @@ package ua.ithillel.lessons.lesson3;
 
 public class TaskOne {
     public static void main(String[] args) {
-        String text = "ZюукяДZіботZ,ежобZощZяZенZ!ьлаксом";
-        String text_replace = text.replace("Z", " ");
-        String[] text_split = text_replace.split(" ");
-        String[] text_reverse = new String[text_split.length];
-        String text_result = "";
-        for(int i=0; i<text_split.length; i++){
-            text_reverse[i] = new StringBuilder(text_split[i]).reverse().toString();
-            text_result += text_reverse[i];
-            if(i<text_split.length-1){
-                text_result += " ";
+        String[] newText = {
+                ",ежоБA,ябетAюрадогалБ",
+                ",меьтяпсарAдерпAьсялоМ",
+                ",яьнахыдAеьтсачсAаЗ",
+                ",ирузалAьтселерпAаЗ",
+                ",ежортсAенмAокAьдубAеН",
+                ",мяьтарбAмиовсAкAяAмеЧ",
+                ",яьнадартсAтоAьвабзИ",
+                ",ерубAвAмечотевсAьдуБ",
+                "абелхAогонщусаН",
+                ",удубAенAадAнешиЛ",
+                ",ыдобовсAагалбAиН",
+                ";ецинметAвA,хазележAВ",
+                "обенAенмAьтедивAйаД",
+                "удучAумонсяAИ",
+                "ыдорирпAйонтремссеБ",
+                ".ясьтивидAонвендесВ",
+                "еримAвA—AовтсежумAйаД",
+                ",онсачесвAмылтевсAьтыБ",
+                ",оледAеовсAьташревС",
+                ",енмAигомопAьтепAИ",
+                ",ерилAйоннемалпAаН",
+                ",онсаркерпAотчA,есвA,есВ",
+                ",олетAиAушудAИ",
+                "!енмигAмоннеремзарAВ",
+                "7191AьрбятнеСA.восюрБAйирелаВ"
+        };
+        String[] newTextAfterReplace = new String[newText.length];
+        String[] newTextAfterRevers = new String[newText.length];
+        String[][] newTextAfterSplit = new String[newText.length][];
+        for (int i=0; i<newText.length; i++){
+            newTextAfterReplace[i] = newText[i].replace('A', ' ');
+            newTextAfterRevers[i] = new StringBuilder(newTextAfterReplace[i]).reverse().toString();
+            newTextAfterSplit[i] = newTextAfterRevers[i].split(" ");
+        }
+
+        StringBuilder[] resultText = new StringBuilder[newTextAfterSplit.length];
+        for (int i=0; i<resultText.length; i++){
+            resultText[i] = new StringBuilder("");
+        }
+        for (int i=0; i<newTextAfterSplit.length; i++){
+            for (int j=0; j<newTextAfterSplit[i].length; j++){
+                resultText[i].append(newTextAfterSplit[i][j]).append(" ");
             }
         }
-        System.out.println(text_result);
+        for (StringBuilder str: resultText){
+            System.out.println(str.toString());
+        }
     }
 }
